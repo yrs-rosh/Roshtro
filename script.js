b00edbfff1da02e79482f50e02cbc5f2beb633f6
rosh <roshankr2110@gmail.com>
2021-11-11 06:08:59 +0530

    11/11 final deployment

--- script.js
+++ script.js
@@ -15,17 +15,17 @@ function getInputValue(){
     alert(`${yourName} are you ready for result`);
     document.getElementById('percentage').innerHTML = `${per}%`;
     if(per>90){
-        document.getElementById("comp").innerHTML = `${yourName} and ${partnerName}, You both are made for each other.`;
+        document.getElementById("comp").innerHTML = `${yourName} and ${partnerName}, You both are made for each other. 😘`;
     }else if(per>80){
-        document.getElementById("comp").innerHTML = `${yourName} and ${partnerName} can be a perfect couple.`;
+        document.getElementById("comp").innerHTML = `${yourName} and ${partnerName} can be a perfect couple. 😍`;
     }else if(per>60){
-        document.getElementById("comp").innerHTML = `${yourName}, you have a good friendship with ${partnerName}. Don't ever think of being in relationship. You may loose your friendship too...  `;
+        document.getElementById("comp").innerHTML = `${yourName}, you have a good friendship with ${partnerName}. Don't ever think of being in relationship. You may loose your friendship too... 🤗 `;
     }else if(per>50){
-        document.getElementById("comp").innerHTML = `${yourName} and ${partnerName} You both seems to be friends who can do anything for each other.`;
+        document.getElementById("comp").innerHTML = `${yourName} and ${partnerName} You both seems to be friends who can do anything for each other. 😊`;
     }else if(per>30){
-        document.getElementById("comp").innerHTML = `${yourName} and ${partnerName} are just friends, Try to spend some time So that you both uderstand each other.`;
+        document.getElementById("comp").innerHTML = `${yourName} and ${partnerName} are just friends, Try to spend some time So that you both uderstand each other. 🤔`;
     }else if(per<=30){
-        document.getElementById("comp").innerHTML = `${yourName}, Try on someone else. ${partnerName} is not for you.`;
+        document.getElementById("comp").innerHTML = `${yourName}, Try on someone else. ${partnerName} is not for you. 😒`;
     }
 
     document.getElementById("percentage").style.display="flex"
